9cb031edcb104b425cb3c1d1e13d278f329c1f8e
Rini Kilend <t.kilend@web.de>
2024-07-11 15:18:48 +0200

    Basic mathematics and showing value types in the console.

---
+++ cloud9-starter.py
@@ -0,0 +1,38 @@
+
+2+2
+
+4-2
+
+2 * 2
+
+4/2
+
+print("Python has three numeric types: int, float, and complex")
+
+myValue=1
+print(myValue)
+print(type(myValue))
+print((str(myValue)) + " is of the data type " + str(type(myValue)))
+
+
+myValues=3.14
+print(myValues)
+print(type(myValues))
+print(str(myValues) + " is of the data type " + str(type(myValues)))
+
+myValuess=5j
+
+print(myValuess)
+
+print(type(myValuess))
+
+print(str(myValuess) + "is of the data type " + str(type(myValuess)))
+
+
+myV4lue=True
+
+print(myV4lue)
+
+print(type(myV4lue))
+
+print(str(myV4lue) + " is of the data type " + str(type(myV4lue)))
