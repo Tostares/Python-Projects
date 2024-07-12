82f43965c03d4415a452dfffb73f8f9f33302d86
Rini Kilend <t.kilend@web.de>
2024-07-12 16:02:12 +0200

    writing an import script to insert single entries into the database

---
+++ importing-into-mysql.py
@@ -0,0 +1,26 @@
+import mysql.connector
+
+#Specify database, user, password as well as where we want to insert what content
+try:
+    connection = mysql.connector.connect(host='localhost',
+                                         database='electronics',
+                                         user='root',
+                                         password='password')
+
+    mySql_insert_query = """INSERT INTO Laptop (Id, Name, Price, Purchase_date) 
+                           VALUES 
+                           (15, 'Lenovo ThinkPad P71', 6459, '2019-08-14') """
+
+    cursor = connection.cursor()
+    cursor.execute(mySql_insert_query)
+    connection.commit()
+    print(cursor.rowcount, "Record inserted successfully into Laptop table")
+    cursor.close()
+
+except mysql.connector.Error as error:
+    print("Failed to insert record into Laptop table {}".format(error))
+
+finally:
+    if connection.is_connected():
+        connection.close()
+        print("MySQL connection is closed")
